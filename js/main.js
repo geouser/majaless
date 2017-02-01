8bed063e12a3834846d077a1111d7047f4422f0e
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2017-02-01 14:38:33 +0200

    f

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ jQuery(document).ready(function($) {
       maxTilt:        20,
       perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
       scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
-      speed:          300,    // Speed of the enter/exit transition.
+      speed:          500,    // Speed of the enter/exit transition.
       transition:     true,   // Set a transition on enter/exit.
       axis:           null,   // What axis should be disabled. Can be X or Y.
       reset:          true,   // If the tilt effect has to be reset on exit.
